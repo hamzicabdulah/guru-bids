502c2956543c722e4f04e46f1c5551e38ad5afc7
Abdulah Hamzic <abdulah.hamzic@thebeaverhead.com>
2018-11-22 15:58:26 +0100

    Main extension functionality complete

---
+++ src/js/content-script.js
@@ -0,0 +1,54 @@
+const axios = require("axios");
+
+/** */
+generateApplicantsModal();
+getApplicantsData().then(applicantsData => {
+  const showApplicantsButton = document.querySelector("#spnTotalApplicant > a")
+  showApplicantsButton.addEventListener("click", addBidAmountInfo(applicantsData));
+});
+/** */
+
+/** */
+function generateApplicantsModal() {
+  location.href = "javascript:AllApplicants(); void 0";
+}
+/** */
+
+/** */
+async function getApplicantsData() {
+  const applicantsDataURL = "https://www.guru.com/pro/JobDetail.aspx/GetJsonForApplicant_ProposalDetails";
+  const response = await axios.post(applicantsDataURL, {
+    ProjectId: +location.pathname.split("/")[3].split("&")[0]
+  });
+  const applicantsData = JSON.parse(response.data.d).Result;
+  return applicantsData;
+}
+/** */
+
+/** */
+function addBidAmountInfo(applicantsData) {
+  return () => {
+    const applicantDivs = document.querySelectorAll("#modalApplicants > .clearfix");
+    applicantDivs.forEach((div, index) => {
+      const applicantBidAmount = applicantsData[index].BidAmount;
+      const applicantBidAmountFormatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0
+      }).format(applicantBidAmount);
+
+      const applicantBidAmountDiv = document.createElement("div");
+      applicantBidAmountDiv.setAttribute("style", `
+        font-weight: 800; 
+        color: black; 
+        opacity: 0.7;
+      `);
+      applicantBidAmountDiv.innerHTML = `Bid Amount: ${applicantBidAmountFormatted}`;
+
+      const bidInfoDiv = div.children[1];
+      bidInfoDiv.append(applicantBidAmountDiv)
+    });
+  }
+}
+/** */
--- webpack.config.js
+++ webpack.config.js
@@ -20,9 +20,10 @@ if (fileSystem.existsSync(secretsPath)) {
 
 var options = {
   entry: {
-    popup: path.join(__dirname, "src", "js", "popup.js"),
-    options: path.join(__dirname, "src", "js", "options.js"),
-    background: path.join(__dirname, "src", "js", "background.js")
+    contentScript: path.join(__dirname, "src", "js", "content-script.js")
+  },
+  chromeExtensionBoilerplate: {
+    notHotReload: ["contentScript"]
   },
   output: {
     path: path.join(__dirname, "build"),
@@ -68,21 +69,6 @@ var options = {
         }))
       }
     }]),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "popup.html"),
-      filename: "popup.html",
-      chunks: ["popup"]
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "options.html"),
-      filename: "options.html",
-      chunks: ["options"]
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "background.html"),
-      filename: "background.html",
-      chunks: ["background"]
-    }),
     new WriteFilePlugin()
   ]
 };
